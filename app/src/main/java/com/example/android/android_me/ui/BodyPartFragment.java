acb746e07286dc0cb71967d7fba55438fc5ae02d
Vicuko <victorc.fernandez@gmail.com>
2019-02-06 14:12:03 +0100

    Implement onCreateView2

--- app/src/main/java/com/example/android/android_me/ui/BodyPartFragment.java
+++ app/src/main/java/com/example/android/android_me/ui/BodyPartFragment.java
@@ -6,6 +6,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.example.android.android_me.R;
+import com.example.android.android_me.data.AndroidImageAssets;
 
 /**
  * Created by Vicuko on 6/2/19.
@@ -24,6 +28,15 @@ public class BodyPartFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+//        Inflate the layout
+        View rootView = inflater.inflate(R.layout.fragment_body_part, container, false);
+
+//        Get a reference to the imageview from the layout
+        ImageView imageView = (ImageView) rootView.findViewById(R.id.body_part_imageview);
+
+//        Set the image resource to display
+        imageView.setImageResource(AndroidImageAssets.getHeads().get(0));
+
+        return rootView;
     }
 }
