c04df9da9e796f636b82edfa5488d15f8b90e34d
Vicuko <victorc.fernandez@gmail.com>
2019-02-06 14:08:01 +0100

    Move TODOs to Fragment and implement constructor and onCreateView method

--- app/src/main/java/com/example/android/android_me/ui/AndroidMeActivity.java
+++ app/src/main/java/com/example/android/android_me/ui/AndroidMeActivity.java
@@ -27,11 +27,6 @@ public class AndroidMeActivity extends AppCompatActivity {
     // TODO (1) Create a layout file that displays one body part image named fragment_body_part.xml - Done
         // This layout should contain a single ImageView
 
-    // TODO (2) Create a new class called BodyPartFragment to display an image of an Android-Me body part
-        // In this class, you'll need to implement an empty constructor and the onCreateView method
-        // TODO (3) Show the first image in the list of head images
-        // Soon, you'll update this image display code to show any image you want
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/com/example/android/android_me/ui/BodyPartFragment.java
+++ app/src/main/java/com/example/android/android_me/ui/BodyPartFragment.java
@@ -1,9 +1,29 @@
 package com.example.android.android_me.ui;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by Vicuko on 6/2/19.
  */
+// TODO (2) Create a new class called BodyPartFragment to display an image of an Android-Me body part
+// In this class, you'll need to implement an empty constructor and the onCreateView method
+// TODO (3) Show the first image in the list of head images
+// Soon, you'll update this image display code to show any image you want
+
 public class BodyPartFragment extends Fragment {
+
+    public BodyPartFragment() {
+        super();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
 }
